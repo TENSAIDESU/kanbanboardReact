f2269bfe07589d68d30a65ff23c91de72a612814
TENSAIDESU <140738963+TENSAIDESU@users.noreply.github.com>
2024-06-11 15:55:34 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { createRoot } from 'react-dom/client';
+import store from "./tasklocal/store";
+import Header from "./Components/header/header";
+import Footer from "./Components/footer/footer";
+import TaskDetails from "./Components/task/task.details";
+import Main from "./Components/main/main";
+import "../src/index.css"
+
+ const App = () => {
+ return (<>
+      <Header/>
+      <Routes> 
+        <Route path='/Kanban-board' element={<Main/>}/>
+        <Route path='/tasks/:taskId' element={<TaskDetails/>}/>
+       </Routes>
+       <Footer/>
+   </>  
+   )
+ };
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+ <Provider store={store}>
+     <BrowserRouter>
+       <App/>
+     </BrowserRouter>
+   </Provider>
+);
+
